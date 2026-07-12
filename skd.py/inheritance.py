0d2b786c5cbf30b59caae49b3409cdbb96403996
sambeet <sambitdash240@gmail.com>
2026-07-12 19:52:37 +0530

    add

---
+++ skd.py/inheritance.py
@@ -0,0 +1,90 @@
+#    multiple inheritance 
+# class employee:
+#     name="sambeet"
+#     dep="cse"
+
+#     def son(self):
+#         print(f"the name is {self.name} and the dep is {self.dep}")
+# class student:
+#     nickname="sandeep"
+#     sec="a"
+#     roll=111
+
+#     def skd(self):
+#         print(f"the name is {self.nickname} ,sec {self.sec} ,roll {self.roll}")
+
+# class popu(employee,student):
+#     company="skd"
+#     def null(self):
+#         print(f"the company {self.company}")
+
+# a=popu()
+# print(a.sec,a.dep,a.roll,a.company)
+# a.son()
+# a.skd()
+# a.null()
+
+# # multilevel inheritance.......
+# class boss1:
+#     a=1
+
+# class boss2(boss1):
+#     b=2
+
+# class boss3(boss2):
+#     c=3
+
+# d=boss3()
+# print(d.a,d.b,d.c)
+
+# e=boss2()
+# print(e.a,e.b)
+
+# # if we use  a simple{} __init__()} function within the MI then we are capable of calling only
+# #  that function, which function is writting inside that class 
+# class boss1:
+#     a=1
+#     def __init__(self):
+#         print("hello boss1")
+
+# class boss2(boss1):
+#     b=2
+#     def __init__(self):
+#         print("hello boss2")
+
+# class boss3(boss2):
+#     c=3
+#     def __init__(self):
+#         print("hello boss3")
+
+# d=boss3()
+# print(d.a,d.b,d.c)
+
+# e=boss2()
+# print(e.a,e.b)
+
+# g=boss1()
+# print(g.a)
+
+# multilevel inheritance with {super().__inti__()}.....
+
+class bmw:
+    name="sambeet"
+    def __init__(self):
+        print(f"the name :{self.name}")
+
+class od(bmw):
+    name2="skd"
+    def __init__(self):
+        print(f"the name :{self.name2}")
+
+class farari(od):
+    name3="pkd"
+    def __init__(self):
+        super().__init__()
+        print(f"the name:{self.name3}")
+
+a=farari()
+print(a.name3,a.name2)        
+
+           
