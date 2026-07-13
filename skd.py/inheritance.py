394f5585e30725e14f5ca50185da3c10e058d4b1
sambeet <sambitdash240@gmail.com>
2026-07-13 19:55:49 +0530

    add

--- skd.py/inheritance.py
+++ skd.py/inheritance.py
@@ -68,23 +68,111 @@
 
 # multilevel inheritance with {super().__inti__()}.....
 
-class bmw:
-    name="sambeet"
-    def __init__(self):
-        print(f"the name :{self.name}")
+# class bmw:
+#     name="sambeet"
+#     def __init__(self):
+#         print(f"the name :{self.name}")
 
-class od(bmw):
-    name2="skd"
-    def __init__(self):
-        print(f"the name :{self.name2}")
+# class od(bmw):
+#     name2="skd"
+#     def __init__(self):
+#         print(f"the name :{self.name2}")
 
-class farari(od):
-    name3="pkd"
-    def __init__(self):
-        super().__init__()
-        print(f"the name:{self.name3}")
+# class farari(od):
+#     name3="pkd"
+#     def __init__(self):
+#         super().__init__()
+#         print(f"the name:{self.name3}")
 
-a=farari()
-print(a.name3,a.name2)        
+# a=farari()
+# print(a.name3,a.name2)        
 
-           
+# class methods......
+
+# class employee:
+#     name="sambeet"
+#     @classmethod
+#     def show(self):
+#         print(f"the class attribute is {self.name}")
+
+# a=employee()
+# a.name="sandeep"
+# print(a.name)
+# a.show()
+
+# operator overlode.......
+
+# class math:
+#     def __init__(self,n):
+#         self.n = n
+
+#     def __add__(self,num):
+#         print(f"the sum is {self.n+num.n}")
+#     def __sub__(self,num):
+#         print(f"the sum is {self.n-num.n}")
+#     def __mul__(self,num):
+#         print(f"the sum is {self.n*num.n}")
+#     def __truediv__(self,num):
+#         print(f"the sum is {self.n/num.n}")
+
+
+
+# a=math(1)
+# b=math(2)
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+# practice set.....
+
+# class twodvector:
+#     def __init__(self,i,j):
+#         self.i=i
+#         self.j=j
+
+#     def show(self):
+#         print(f"{self.i}i+{self.j}j")
+# class threedvector(twodvector):
+#     def __init__(self,i,j,k):
+#         super().__init__(i,j)
+#         self.k=k
+
+#     def show(self):
+#         print(f"{self.i}i+{self.j}j+{self.k}k")
+
+# a=twodvector(2,3)
+# b=threedvector(2,3,4) 
+# a.show()
+# b.show()             
+        
+
+# 2.....
+
+# class complex:
+#     def __init__(self,a,b):
+#         self.a=a
+        
+        
+
+#     @property    
+#     def __add__(self,num):
+#         return self.a+num.a
+        
+    
+#     def show(self):
+#         print(f"{self.a}+{self.b}j")
+
+# class complexn(complex):
+#     def __init__(self,a,b):
+#         super().__init__(a,b)
+#         self.b=b
+
+#     def __mul__(self,num):
+#          return self.b+num.b
+    
+#     def show(self):
+#         print(f"{self.a}*{self.b}j")
+
+# s=complexn(8,7)
+# s.show()
